ea8689f3eae9f80010e4e1e0111f3974921e0da7
Thethela Faltein <thethelafaltein@gmail.com>
2019-09-27 14:20:54 +0200

    Init dev

---
+++ src/main/Application.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Application {
+    // --- command line ---
+    // -algorithm [ga | sa | aco | pso | best-algorithm] -configuration [default | best] -search_best_configuration
+    public static void main(String... args)
+    {
+
+    }
+}
