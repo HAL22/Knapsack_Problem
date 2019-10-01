feadb0ec136604fc33c496b0a19b5596280a2b8a
Thethela Faltein <thethelafaltein@gmail.com>
2019-10-01 20:43:04 +0200

    Achiveve a fitness greater than the optimal

--- src/algorithm/ga/base/Chromosome.java
+++ src/algorithm/ga/base/Chromosome.java
@@ -1,5 +1,143 @@
 package algorithm.ga.base;
 
-public class Chromosome
+import main.Configuration;
+import main.KnapsackItem;
+
+import java.util.ArrayList;
+
+public class Chromosome implements Comparable<Chromosome>
 {
+
+    private String gene;
+    private int fitness;
+    public ArrayList<KnapsackItem>Knapsack;
+    public double probability ; // used by the Roulette wheel selection
+
+    public Chromosome(String gene)
+    {
+        this.gene = gene;
+        Knapsack = null;
+        fitness =0;
+        probability = 0.0;
+
+    }
+
+    public String getGene() {
+        return gene;
+    }
+
+    public void setGene(String gene) {
+        this.gene = gene;
+    }
+
+    public int getFitness() {
+        return fitness;
+    }
+
+    public void setFitness(int fitness) {
+        this.fitness = fitness;
+    }
+
+    public ArrayList<KnapsackItem> getKnapsack() {
+        return Knapsack;
+    }
+
+    public void setKnapsack(ArrayList<KnapsackItem> knapsack) {
+        Knapsack = knapsack;
+    }
+
+
+    // Before you set the fitness make sure you set the knapsack array
+
+    public void setFitness()
+    {
+        this.fitness = calculateFintness();
+    }
+
+    public double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(double probability) {
+        this.probability = probability;
+    }
+
+    public static Chromosome generateRandom()
+    {
+        char charArray[] = new char[Configuration.instance.numberOfItems];
+
+        for(int i=0;i<charArray.length;i++)
+        {
+            if(Configuration.instance.randomGenerator.nextFloat()>0.8)
+            {
+                charArray[i]='1';
+            }
+            else
+                {
+                    charArray[i]='0';
+                }
+
+
+
+
+        }
+
+
+
+
+        return new Chromosome(String.valueOf(charArray));
+    }
+
+    public int calculateFintness()
+    {
+        int fitValue =0;
+        int weightValue =0;
+
+        for(int i=0;i<this.gene.length();i++)
+        {
+            if(this.gene.charAt(i)=='1')
+            {
+                fitValue = fitValue + Knapsack.get(i).getValue();
+                weightValue = weightValue + Knapsack.get(i).getWeight();
+            }
+        }
+
+        //System.out.println(weightValue);
+
+
+
+        if(weightValue>Configuration.instance.maximumCapacity)
+        {
+            return 0;
+        }
+        else
+            {
+                return fitValue;
+            }
+    }
+
+    public int compareTo(Chromosome chromosome) {
+        if (fitness < chromosome.getFitness())
+            return -1;
+
+        if (fitness > chromosome.getFitness())
+            return 1;
+
+        return 0;
+    }
+
+
+    public int hashCode()
+    {
+        return (gene + fitness).hashCode();
+    }
+
+
+
+
+
+
+
+
+
 }
--- src/algorithm/ga/base/Population.java
+++ src/algorithm/ga/base/Population.java
@@ -1,4 +1,134 @@
 package algorithm.ga.base;
 
-public class Population {
+import algorithm.ga.evolution.crossover.OnePoint;
+import algorithm.ga.evolution.mutation.BitFlip;
+import algorithm.ga.evolution.mutation.Inversion;
+import algorithm.ga.evolution.selection.Tournament;
+import main.Configuration;
+import main.KnapsackItem;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Population
+{
+    private double elitismRatio;
+    private double mutationRatio;
+    private double crossoverRatio;
+    private Chromosome[] population;
+    private int numberOfCrossoverOperations = 0;
+    private int numberOfMutationOperations = 0;
+    private ArrayList<KnapsackItem> Knapsack;
+
+    public Population(int size, double crossoverRatio, double elitismRatio, double mutationRatio,ArrayList<KnapsackItem> Knapsack) {
+        this.crossoverRatio = crossoverRatio;
+        this.elitismRatio = elitismRatio;
+        this.mutationRatio = mutationRatio;
+        this.Knapsack = Knapsack;
+        population = new Chromosome[size];
+
+
+        for (int i = 0; i < size; i++)
+        {
+            population[i] = Chromosome.generateRandom();
+            population[i].setKnapsack(Knapsack);
+            population[i].setFitness();
+        }
+
+        Arrays.sort(population, Collections.reverseOrder());
+
+    }
+
+    public int getNumberOfCrossoverOperations() {
+        return numberOfCrossoverOperations;
+    }
+
+    public int getNumberOfMutationOperations() {
+        return numberOfMutationOperations;
+    }
+
+    public Chromosome[] getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Chromosome[] population) {
+        this.population = population;
+    }
+
+    public void evolve()
+    {
+        Chromosome[] chromosomeArray = new Chromosome[population.length];
+        int index = (int) Math.round(population.length * elitismRatio);
+        System.arraycopy(population, 0, chromosomeArray, 0, index);
+
+        while (index < chromosomeArray.length)
+        {
+            if (Configuration.instance.randomGenerator.nextFloat() <= crossoverRatio)
+            {
+                Chromosome[] parents = Tournament.selectParent(population);
+                Chromosome parent2 = parents[1];
+                Chromosome parent1 = parents[0];
+
+                System.out.println(parent1.getGene());
+                System.out.println(parent2.getGene());
+
+                System.out.println();
+
+                Chromosome[] children = OnePoint.doCrossover(parent1,parent2);
+                numberOfCrossoverOperations++;
+
+                if (Configuration.instance.randomGenerator.nextFloat() <= mutationRatio)
+                {
+                    Chromosome c =  new Chromosome(BitFlip.doMutation(children[0].getGene().toCharArray()));
+                    chromosomeArray[(index++)] = c;
+                    numberOfMutationOperations++;
+
+                }
+                else
+                    chromosomeArray[(index++)] = children[0];
+
+                if (index < chromosomeArray.length)
+
+                    if (Configuration.instance.randomGenerator.nextFloat() <= mutationRatio)
+                    {
+                        Chromosome c =  new Chromosome(BitFlip.doMutation(children[1].getGene().toCharArray()));
+                        chromosomeArray[index] = c;
+                        numberOfMutationOperations++;
+                    }else
+                        chromosomeArray[index] = children[1];
+
+
+            }
+
+            else if (Configuration.instance.randomGenerator.nextFloat() <= mutationRatio)
+            {
+                Chromosome c= new Chromosome(BitFlip.doMutation(population[index].getGene().toCharArray()));
+                c.setKnapsack(Knapsack);
+                c.setFitness();
+                chromosomeArray[index] = c;
+                numberOfMutationOperations++;
+            } else {
+                chromosomeArray[index] = population[index];
+            }
+
+            index++;
+
+
+
+        }
+
+
+        Arrays.sort(chromosomeArray,Collections.reverseOrder());
+        population = chromosomeArray;
+
+
+    }
+
+
+
+
+
+
 }
